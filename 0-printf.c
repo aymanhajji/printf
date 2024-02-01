80e2b92228ff383e64eeace0ea3f8c7178df34ad
aymanhajji <hajjiayman03@gmail.com>
2024-02-01 23:21:41 +0100

    Adding a condition before the while loop

--- 0-printf.c
+++ 0-printf.c
@@ -16,6 +16,10 @@ int _printf(const char *format, ...)
 
 	va_start(argsDest, format);
 	count = 0;
+	if (*format == NULL)
+	{
+		return (-1);
+	}
 	while (*format != '\0')
 	{
 		if (*format == '%')
@@ -52,3 +56,4 @@ int _printf(const char *format, ...)
 	va_end(argsDest);
 	return (count);
 }
+
